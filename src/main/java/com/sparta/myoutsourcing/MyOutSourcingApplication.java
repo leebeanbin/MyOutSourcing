8de7b65271f2a31fb8697046a0514643db07164c
leebeanbin <wjdqlsdy1004@naver.com>
2024-07-01 19:38:15 +0900

    init settings

---
+++ src/main/java/com/sparta/myoutsourcing/MyOutSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myoutsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOutSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOutSourcingApplication.class, args);
+    }
+
+}

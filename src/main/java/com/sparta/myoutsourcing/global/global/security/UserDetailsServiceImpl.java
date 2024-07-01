20fad4300347b9360cda8be0541738656492ca87
leebeanbin <wjdqlsdy1004@naver.com>
2024-07-01 19:41:17 +0900

    init settings(2)

---
+++ src/main/java/com/sparta/myoutsourcing/global/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sparta.myoutsourcing.global.global.security;
+
+import com.sparta.codeplanet.product.entity.User;
+import com.sparta.codeplanet.product.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("This \"%s\" does not exist.", username)));
+
+        return new UserDetailsImpl(user);
+    }
+}
